fad08e20003dd2cbf42cf25f95d01efffe38ee55
Josepe04 <josepe04@ucm.es>
2016-12-27 18:56:50 +0100

    Borrando cosas que sobraban

--- prEtica/src/main/webapp/resources/arcade/arcade.js
+++
@@ -1,9 +0,0 @@
-function elige(id, nombre, oro, experiencia, nivel){
-	$("button").attr("value",id);
-	$(".descripcion").show();
-	
-    $("#idNombre").text(nombre);
-    $("#idOro").text(oro);
-    $("#idExperiencia").text(experiencia);
-    $("#idNivel").text(nivel);
-}
